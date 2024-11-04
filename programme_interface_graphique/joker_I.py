5cccef88628e651fa25a81db9b3787ef811cc0af
Deneuvi <130739093+Deneuvi@users.noreply.github.com>
2024-11-04 11:11:55 +0100

    Update joker_I.py

--- programme_interface_graphique/joker_I.py
+++ programme_interface_graphique/joker_I.py
@@ -1,34 +1,31 @@
 from jeton_I import Jeton 
 
 class Joker(Jeton):
-    """Représente un joker dans le jeu Rummikub.
+    """Représente un joker dans le jeu, hérite de la classe Jeton."""
     
-    Hérite de la classe Jeton et modifie certains comportements pour
-    représenter un joker spécifique.
-    """
-
     def __init__(self, joker_image):
-        """Initialise un joker avec l'image fournie.
+        """
+        Initialise un joker avec une image spécifique.
         
         Paramètres :
-        - joker_image : une chaîne de caractères représentant le chemin de l'image du joker.
-        
-        Utilise None pour le nombre car le joker ne devrait pas avoir de valeur spécifique.
+        - joker_image : le chemin de l'image représentant le joker.
         """
         super().__init__(None, 'Joker', joker_image)  # Utiliser None pour le nombre car le joker ne devrait pas avoir de valeur spécifique
 
     def __str__(self):
-        """Renvoie une représentation sous forme de chaîne du joker.
+        """
+        Renvoie une représentation sous forme de chaîne du joker.
         
         Retourne :
-        - Une chaîne contenant le texte "Joker".
+        - "Joker" pour indiquer qu'il s'agit d'un joker.
         """
         return "Joker"  # Affiche "Joker" à la place de "0 Joker"
 
     def est_joker(self):
-        """Détermine si le jeton est un joker.
+        """
+        Indique que l'objet est un joker.
         
         Retourne :
-        - True car cette méthode indique que c'est un joker.
+        - True, car cette méthode confirme que c'est un joker.
         """
         return True  # Méthode pour indiquer que c'est un joker
